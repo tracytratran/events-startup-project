ef2de4b7c211b0169e5647e23f17805b8d02f307
Tracy Trà Trần <tratran.315@gmail.com>
2026-04-22 19:50:04 +0200

    Add styling to make Footer stay at the bottom of every page

--- app/src/components/Layout/Layout.jsx
+++ app/src/components/Layout/Layout.jsx
@@ -2,13 +2,14 @@ import { Link, Outlet } from "react-router-dom";
 import hyfLogo from "../../assets/hyf.svg";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import styles from "./Layout.module.css";
 
 export default function Layout() {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header />
 
-      <main>
+      <main className={styles.flexGrow1}>
         <Outlet />
       </main>
 
