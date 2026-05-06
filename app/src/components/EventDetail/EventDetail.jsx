63d87b8f2cf64217ee21d98a624ee30ab79d863e
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-06 16:26:46 +0200

    chore: Delete comments in EventDetail.jsx

--- app/src/components/EventDetail/EventDetail.jsx
+++ app/src/components/EventDetail/EventDetail.jsx
@@ -4,10 +4,6 @@ import events from "../../data/events.js";
 import styles from "./EventDetail.module.css";
 import useEventById from "../../hooks/useEventById.jsx";
 
-// TODO: display at least date, time, venue, city, and description for one event
-// TODO: use useParams() to get the event id from the URL
-// TODO: fetch the event from GET /events/:id instead of using mock data
-
 export default function EventDetail() {
   const { id } = useParams();
   const { event: eventToDisplay, loading, error } = useEventById(id);
