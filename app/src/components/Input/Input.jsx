55ec016bdcbac57e5ab3be1e2514c69a81fcceaf
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-06 22:10:46 +0200

    Add validation for PasswordInput component

--- app/src/components/Input/Input.jsx
+++ app/src/components/Input/Input.jsx
@@ -1,6 +1,6 @@
 import styles from "./Input.module.css";
 
-export function EmailInput({ name }) {
+export function EmailInput({ name, handleOnChange }) {
   return (
     <input
       type="email"
@@ -8,19 +8,25 @@ export function EmailInput({ name }) {
       name={name}
       placeholder="you@example.com"
       pattern="[a-z0-9._%\+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+      onChange={handleOnChange}
       required
       className={styles.input}
     />
   );
 }
 
-export function PasswordInput({ name }) {
+export function PasswordInput({ name, handleOnChange }) {
   return (
     <input
       type="password"
       id={name}
       name={name}
       placeholder="••••••••"
+      minlength="8"
+      maxlength="64"
+      pattern="(?=.*[A-Z])(?=.*[0-9]).{8,}"
+      title="At least 8 characters, one uppercase letter, and one number"
+      onChange={handleOnChange}
       required
       className={styles.input}
     />
