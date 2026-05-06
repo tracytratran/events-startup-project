6fbdbefe92fe1d12802357f114f76c94c0ac1f95
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-06 20:41:50 +0200

    Build the UI for register

--- app/src/components/Register/Register.jsx
+++ app/src/components/Register/Register.jsx
@@ -3,13 +3,35 @@
 // TODO: show a clear error message if registration fails
 // TODO: redirect to the event list on success
 
+// TODO: build a login form with relevant fields
+// TODO: call login(email, password) from useAuth() on submit
+// TODO: show a clear error message if login fails
+// TODO: redirect to the event list on success
+import FormField from "../FormField/FormField";
+import { EmailInput, PasswordInput } from "../Input/Input";
+import Footer from "../Layout/Footer";
+import styles from "./Register.module.css";
+
 export default function Register() {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <h1>Register</h1>
-      <p>Register form — coming soon.</p>
+    <div className={styles.formWrapper}>
+      <form className={styles.registerForm}>
+        <h1 className={styles.formTitle}>Register here</h1>
+        <hr className={styles.divider} />
+
+        <FormField name="email" label="Email">
+          <EmailInput name="email" />
+        </FormField>
+
+        <FormField name="password" label="Password">
+          <PasswordInput name="password" />
+        </FormField>
+
+        <button type="submit" className={styles.registerBtn}>
+          Register
+        </button>
+      </form>
+      <Footer />
     </div>
   );
 }
