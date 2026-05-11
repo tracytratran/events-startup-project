1d41eccf17b0653720e5943c68b02bc9dd7bb49f
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-11 23:04:28 +0200

    chore: Remove the fragment parent in FilterOption

--- app/src/components/FilterOption/FilterOption.jsx
+++ app/src/components/FilterOption/FilterOption.jsx
@@ -2,18 +2,16 @@ import styles from "./FilterOption.module.css";
 
 export default function FilterOption({ filterTitle, filterOptions, onChange }) {
   return (
-    <>
-      <div className={styles.filterSection}>
-        <h3 className={styles.filterTitle}>{filterTitle}</h3>
-        <div className={styles.filterOptions}>
-          {filterOptions.map((label) => (
-            <label key={label} className={styles.filterOption}>
-              <input type="checkbox" name={label} onChange={onChange} />
-              <span className={styles.filterLabel}>{label}</span>
-            </label>
-          ))}
-        </div>
+    <div className={styles.filterSection}>
+      <h3 className={styles.filterTitle}>{filterTitle}</h3>
+      <div className={styles.filterOptions}>
+        {filterOptions.map((label) => (
+          <label key={label} className={styles.filterOption}>
+            <input type="checkbox" name={label} onChange={onChange} />
+            <span className={styles.filterLabel}>{label}</span>
+          </label>
+        ))}
       </div>
-    </>
+    </div>
   );
 }
