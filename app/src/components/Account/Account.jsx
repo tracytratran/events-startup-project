f1a1efcaeab8d5c56397dadbfb5baba742f318b2
Tracy Trà Trần <tratran.315@gmail.com>
2026-05-20 14:01:37 +0200

    Add order detail page in Account

--- app/src/components/Account/Account.jsx
+++ app/src/components/Account/Account.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { useOrder } from "../../context/OrderContext";
+import OrderItem from "../Order/OrderItem";
 import styles from "./Account.module.css";
 
 export default function Account() {
-  return <h1 className={styles.title}>My Account</h1>;
+  const { getOrders } = useOrder();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrders() {
+      const userOrders = await getOrders();
+      setOrders(userOrders);
+    }
+
+    fetchOrders();
+  }, []);
+
+  return (
+    <div className={styles.orderWrapper}>
+      <h1 className={styles.title}>My order history</h1>
+
+      <ul className={styles.orderList}>
+        {orders.map((order) => (
+          <OrderItem key={order.id} id={order.id} events={order.events} />
+        ))}
+      </ul>
+    </div>
+  );
 }
