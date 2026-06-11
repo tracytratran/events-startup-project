2045a96069f01c1980691b0cd940e7ad00d7986b
Tracy Trà Trần <tratran.315@gmail.com>
2026-06-11 13:03:33 +0200

    chore: Clean up template files and update README file

--- templates/app/app-vite-vanilla/src/api.js
+++
@@ -1,4 +0,0 @@
-// This helper function returns the full API URL for a given route based on the environment variable `VITE_API_URL` you have set.
-export default function api(route) {
-  return `${import.meta.env.VITE_API_URL}/api${route}`;
-}
